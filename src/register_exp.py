929fd025395d725a571396b6d0bf04e0e938494d
Domingo Manubens-Gil <domingo.manubens@ic3.cat>
2014-05-27 13:23:54 +0200

    Register experiment: minor corrections, and ocean diagnostics added

--- src/register_exp.py
+++ src/register_exp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-"""Functions for registering in the DB the commit SHA of the git projects version.
-Use these functions for registering git version of templates, model and ocean diagnosics under development."""
+"""Functions for registering in the DB the commit SHA of the GIT projects version.
+Use these functions for registering GIT version of templates, model and ocean diagnosics under development."""
 from dir_config import LOCAL_ROOT_DIR, DB_DIR, DB_FILE, DB_NAME
 import argparse
 from os import path
@@ -54,13 +54,13 @@ def get_experiment(name):
 	close_conn(conn, cursor)
 	return row
 
-def set_experiment(name, model_name, model_branch, template_name, template_branch):
+def set_experiment(name, model_name, model_branch, template_name, template_branch, ocean_diagnostics_branch):
 	check_db()
 	name = check_name(name)
 
 	(conn, cursor) = open_conn()
 	try:
-		cursor.execute('update experiment set type=:model_name, model_branch=:model_branch, template_name=:template_name, template_branch=:template_branch where name=:name', {'name':name, 'model_name':model_name, 'model_branch':model_branch, 'template_name':template_name, 'template_branch':template_branch})
+		cursor.execute('update experiment set type=:model_name, model_branch=:model_branch, template_name=:template_name, template_branch=:template_branch, ocean_diagnostics_branch=:ocean_diagnostics_branch where name=:name', {'name':name, 'model_name':model_name, 'model_branch':model_branch, 'template_name':template_name, 'template_branch':template_branch, 'ocean_diagnostics_branch':ocean_diagnostics_branch})
 	except sql.IntegrityError:
 		close_conn(conn, cursor)
 		print 'The experiment name %s - %s already exists!!!' % (name, exp_type)
@@ -73,10 +73,12 @@ def set_experiment(name, model_name, model_branch, template_name, template_branc
 
 
 def register_sha(expid, save):
-	"""Function to register in the DB the commit SHA of the template and the model project versions."""
+	"""Function to register in the DB the commit SHA of the template, model and ocean diagnostics project versions."""
 	exp = get_experiment(expid)
 	print exp
-	# check expdef_cxxx
+
+	# check model
+	# check expdef_cxxx ?
 	model_name = exp[2]
 	(status1, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/model/")
 	(status2, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/model/" + "; " + "git rev-parse --abbrev-ref HEAD")
@@ -96,8 +98,9 @@ def register_sha(expid, save):
 		exit(1)
 	model_branch_sha = model_branch + " " + model_sha
 
-	# check expdef_cxxx
-	template_name = exp[5]
+	# check templates
+	# check expdef_cxxx ?
+	template_name = exp[7]
 	(status1, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/templates/")
 	(status2, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/templates/" + "; " + "git rev-parse --abbrev-ref HEAD")
 	if (status1 == 0 and status2 == 0):
@@ -116,8 +119,28 @@ def register_sha(expid, save):
 		exit(1)
 	template_branch_sha = template_branch + " " + template_sha
 
+	# check ocean diagnostics
+	(status1, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/ocean_diagnostics/")
+	(status2, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/ocean_diagnostics/" + "; " + "git rev-parse --abbrev-ref HEAD")
+	if (status1 == 0 and status2 == 0):
+		ocean_diagnostics_branch = output
+		print "Ocean diagnostics branch is: " + ocean_diagnostics_branch
+	else:
+		print "Failed to retrieve ocean diagnostics branch..."
+		exit(1)
+	(status1, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/ocean_diagnostics/")
+	(status2, output) = getstatusoutput("cd " + LOCAL_ROOT_DIR + "/" + expid + "/git/ocean_diagnostics/" + "; " + "git rev-parse HEAD")
+	if (status1 == 0 and status2 == 0):
+		ocean_diagnostics_sha = output
+		print "Ocean diagnostics SHA is: " + ocean_diagnostics_sha
+	else: 
+		print "Failed to retrieve ocean diagnostics SHA..."
+		exit(1)
+	ocean_diagnostics_branch_sha = ocean_diagnostics_branch + " " + ocean_diagnostics_sha
+	
+	# register changes
 	if (save):
-		set_experiment(expid, model_name, model_branch_sha, template_name, template_branch_sha)
+		set_experiment(expid, model_name, model_branch_sha, template_name, template_branch_sha, ocean_diagnostics_branch_sha)
 		print "SHA for model and template succesfully registered to the database."
 	else:
 		print "Changes NOT registered to the database..."
@@ -127,12 +150,12 @@ def register_sha(expid, save):
 ####################
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(description='Clean autosubmit finalised experiments directory given an experiment identifier')
+	parser = argparse.ArgumentParser(description='Register experiment GIT directory changes to database, given an experiment identifier')
 	parser.add_argument('-e', '--expid', required=True, nargs = 1, help='Give an experiment identifier...')
 	parser.add_argument('-s', '--save', action="store_true", default=False, help='Save changes to database')
 	args = parser.parse_args()
 	if args.expid is None:
 		parser.error("Missing expid.")
 	
-	print "Looking for model and template commit SHA..."
+	print "Looking for model, templates and ocean_diagnostics commit SHA..."
 	register_sha(args.expid[0], args.save)
