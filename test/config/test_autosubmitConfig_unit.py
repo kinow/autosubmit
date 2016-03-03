6e2d537ee9693a4861cc69f2987c614356f66d45
jlope2 <joan.lopez@bsc.es>
2016-03-03 17:44:12 +0100

    Added new unit tests for autosubmitconfig class

---
+++ test/config/test_autosubmitConfig_unit.py
@@ -0,0 +1,197 @@
+from unittest import TestCase
+from unittest import skip
+from autosubmit.config.config_common import AutosubmitConfig
+from autosubmit.config.parser_factory import ConfigParserFactory
+from mock import Mock
+import os
+try:
+    # noinspection PyCompatibility
+    from configparser import SafeConfigParser
+except ImportError:
+    # noinspection PyCompatibility
+    from ConfigParser import SafeConfigParser
+
+
+class TestAutosubmitConfig(TestCase):
+
+    any_expid = 'a000'
+
+    # dummy values for tests
+    section = 'any-section'
+    option = 'any-option'
+
+    def setUp(self):
+        self.config = AutosubmitConfig(self.any_expid, FakeBasicConfig, ConfigParserFactory())
+
+    def test_get_parser(self):
+        # arrange
+        file_path = 'dummy/file/path'
+
+        parser_mock = Mock(spec=SafeConfigParser)
+        parser_mock.read = Mock()
+
+        factory_mock = Mock(spec=ConfigParserFactory)
+        factory_mock.create_parser = Mock(return_value=parser_mock)
+
+        # act
+        returned_parser = AutosubmitConfig.get_parser(factory_mock, file_path)
+
+        # assert
+        self.assertTrue(isinstance(returned_parser, SafeConfigParser))
+        factory_mock.create_parser.assert_called_with()
+        parser_mock.read.assert_called_with(file_path)
+
+    def test_get_option(self):
+        # arrange
+        section = 'any-section'
+        option = 'any-option'
+        default = 'dummy-default'
+        option_value = 'dummy-value'
+
+        parser_mock = self._create_parser_mock(True, option_value)
+
+        # act
+        returned_option = AutosubmitConfig.get_option(parser_mock, section, option, default)
+
+        # assert
+        parser_mock.has_option.assert_called_once_with(section, option)
+        self.assertTrue(isinstance(returned_option, str))
+        self.assertNotEqual(default, returned_option)
+        self.assertEqual(option_value, returned_option)
+
+    def test_get_option_case_default(self):
+        # arrange
+        section = 'any-section'
+        option = 'any-option'
+        default = 'dummy-default'
+
+        parser_mock = self._create_parser_mock(False)
+
+        # act
+        returned_option = AutosubmitConfig.get_option(parser_mock, section, option, default)
+
+        # assert
+        parser_mock.has_option.assert_called_once_with(section, option)
+        self.assertTrue(isinstance(returned_option, str))
+        self.assertEqual(default, returned_option)
+
+    def test_experiment_file(self):
+        self.assertEqual(self.config.experiment_file,
+                         os.path.join(FakeBasicConfig.LOCAL_ROOT_DIR, self.any_expid, "conf",
+                                             "expdef_" + self.any_expid + ".conf"))
+
+    def test_platforms_file(self):
+        self.assertEqual(self.config.platforms_file,
+                         os.path.join(FakeBasicConfig.LOCAL_ROOT_DIR, self.any_expid, "conf",
+                                             "platforms_" + self.any_expid + ".conf"))
+
+    def test_project_file(self):
+        self.assertEqual(self.config.project_file,
+                         os.path.join(FakeBasicConfig.LOCAL_ROOT_DIR, self.any_expid, "conf",
+                                             "proj_" + self.any_expid + ".conf"))
+
+    def test_jobs_file(self):
+        self.assertEqual(self.config.jobs_file,
+                         os.path.join(FakeBasicConfig.LOCAL_ROOT_DIR, self.any_expid, "conf",
+                                             "jobs_" + self.any_expid + ".conf"))
+
+    @skip("pending refactor")
+    def test_get_project_dir(self):
+        self.assertEqual(self.config.get_project_dir(),
+                         os.path.join(FakeBasicConfig.LOCAL_ROOT_DIR, self.any_expid, FakeBasicConfig.LOCAL_PROJ_DIR,
+                                     self.config.get_project_destination()))
+
+    def test_get_wallclock(self):
+        # arrange
+        expected_value = '00:05'
+        config, parser_mock = self._arrange_config(expected_value)
+        # act
+        returned_value = config.get_wallclock(self.section)
+        # assert
+        self._assert_get_option(parser_mock, 'WALLCLOCK', expected_value, returned_value, str)
+
+    def test_get_processors(self):
+        # arrange
+        expected_value = 99999
+        config, parser_mock = self._arrange_config(expected_value)
+        # act
+        returned_value = config.get_processors(self.section)
+        # assert
+        self._assert_get_option(parser_mock, 'PROCESSORS', expected_value, returned_value, int)
+
+    def test_get_threads(self):
+        # arrange
+        expected_value = 99999
+        config, parser_mock = self._arrange_config(expected_value)
+        # act
+        returned_value = config.get_threads(self.section)
+        # assert
+        self._assert_get_option(parser_mock, 'THREADS', expected_value, returned_value, int)
+
+    def test_get_tasks(self):
+        # arrange
+        expected_value = 99999
+        config, parser_mock = self._arrange_config(expected_value)
+        # act
+        returned_value = config.get_tasks(self.section)
+        # assert
+        self._assert_get_option(parser_mock, 'TASKS', expected_value, returned_value, int)
+
+    def test_get_memory(self):
+        # arrange
+        expected_value = 99999
+        config, parser_mock = self._arrange_config(expected_value)
+        # act
+        returned_value = config.get_memory(self.section)
+        # assert
+        self._assert_get_option(parser_mock, 'MEMORY', expected_value, returned_value, int)
+
+    def test_check_exists_case_true(self):
+        # arrange
+        parser_mock = self._create_parser_mock(True)
+        # act
+        exists = AutosubmitConfig.check_exists(parser_mock, self.section, self.option)
+        # assert
+        parser_mock.has_option.assert_called_once_with(self.section, self.option)
+        self.assertTrue(exists)
+
+    def test_check_exists_case_false(self):
+        # arrange
+        parser_mock = self._create_parser_mock(False)
+        # act
+        exists = AutosubmitConfig.check_exists(parser_mock, self.section, self.option)
+        # assert
+        parser_mock.has_option.assert_called_once_with(self.section, self.option)
+        self.assertFalse(exists)
+
+    def _assert_get_option(self, parser_mock, option, expected_value, returned_value, expected_type):
+        self.assertTrue(isinstance(returned_value, expected_type))
+        self.assertEqual(expected_value, returned_value)
+        parser_mock.has_option.assert_called_once_with(self.section, option)
+
+    def _arrange_config(self, option_value):
+        # arrange
+        parser_mock = Mock(spec=SafeConfigParser)
+        parser_mock.has_option = Mock(return_value=True)
+        parser_mock.get = Mock(return_value=option_value)
+        factory_mock = Mock(spec=ConfigParserFactory)
+        factory_mock.create_parser = Mock(return_value=parser_mock)
+        config = AutosubmitConfig(self.any_expid, FakeBasicConfig, factory_mock)
+        config.reload()
+        return config, parser_mock
+
+    def _create_parser_mock(self, has_option, returned_option=None):
+        parser_mock = Mock(spec=SafeConfigParser)
+        parser_mock.has_option = Mock(return_value=has_option)
+        parser_mock.get = Mock(return_value=returned_option)
+        return parser_mock
+
+class FakeBasicConfig:
+            DB_DIR = '/dummy/db/dir'
+            DB_FILE = '/dummy/db/file'
+            DB_PATH = '/dummy/db/path'
+            LOCAL_ROOT_DIR = '/dummy/local/root/dir'
+            LOCAL_TMP_DIR = '/dummy/local/temp/dir'
+            LOCAL_PROJ_DIR = '/dummy/local/proj/dir'
+            DEFAULT_PLATFORMS_CONF = ''
+            DEFAULT_JOBS_CONF = ''
