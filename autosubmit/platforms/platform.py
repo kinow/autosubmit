691b44982e1f73680627d2ab75da980de2e647a2
Javier Vegas-Regidor <javier.vegas@bsc.es>
2016-04-01 17:15:01 +0200

    Removed traces from python command

--- autosubmit/platforms/platform.py
+++ autosubmit/platforms/platform.py
@@ -382,7 +382,7 @@ class Platform:
         if job.type == Type.BASH:
             binary = 'source'
         elif job.type == Type.PYTHON:
-            binary = 'python -m trace --trace'
+            binary = 'python '
         elif job.type == Type.R:
             binary = 'Rscript'
 
