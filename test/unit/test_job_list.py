03b52b147f6044ef31fe2f74513505a293f2fadc
jlope2 <joan.lopez@bsc.es>
2016-04-07 13:25:34 +0200

    Added new tests to the unitary test suite of JobList class

--- test/unit/test_job_list.py
+++ test/unit/test_job_list.py
@@ -85,6 +85,90 @@ class TestJobList(TestCase):
         self.assertEquals(1, len(queuing))
         self.assertTrue(self.queuing_job in queuing)
 
+    def test_get_failed_returns_only_the_failed(self):
+        failed = self.job_list.get_failed()
+
+        self.assertEquals(4, len(failed))
+        self.assertTrue(self.failed_job in failed)
+        self.assertTrue(self.failed_job2 in failed)
+        self.assertTrue(self.failed_job3 in failed)
+        self.assertTrue(self.failed_job4 in failed)
+
+    def test_get_ready_returns_only_the_ready(self):
+        ready = self.job_list.get_ready()
+
+        self.assertEquals(3, len(ready))
+        self.assertTrue(self.ready_job in ready)
+        self.assertTrue(self.ready_job2 in ready)
+        self.assertTrue(self.ready_job3 in ready)
+
+    def test_get_waiting_returns_only_which_are_waiting(self):
+        waiting = self.job_list.get_waiting()
+
+        self.assertEquals(2, len(waiting))
+        self.assertTrue(self.waiting_job in waiting)
+        self.assertTrue(self.waiting_job2 in waiting)
+
+    def test_get_unknown_returns_only_which_are_unknown(self):
+        unknown = self.job_list.get_unknown()
+
+        self.assertEquals(1, len(unknown))
+        self.assertTrue(self.unknown_job in unknown)
+
+    def test_get_in_queue_returns_only_which_are_queuing_submitted_and_running(self):
+        in_queue = self.job_list.get_in_queue()
+
+        self.assertEquals(6, len(in_queue))
+        self.assertTrue(self.queuing_job in in_queue)
+        self.assertTrue(self.running_job in in_queue)
+        self.assertTrue(self.running_job2 in in_queue)
+        self.assertTrue(self.submitted_job in in_queue)
+        self.assertTrue(self.submitted_job2 in in_queue)
+        self.assertTrue(self.submitted_job3 in in_queue)
+
+    def test_get_not_in_queue_returns_only_which_are_waiting_and_ready(self):
+        not_in_queue = self.job_list.get_not_in_queue()
+
+        self.assertEquals(5, len(not_in_queue))
+        self.assertTrue(self.waiting_job in not_in_queue)
+        self.assertTrue(self.waiting_job2 in not_in_queue)
+        self.assertTrue(self.ready_job in not_in_queue)
+        self.assertTrue(self.ready_job2 in not_in_queue)
+        self.assertTrue(self.ready_job3 in not_in_queue)
+
+    def test_get_finished_returns_only_which_are_completed_and_failed(self):
+        finished = self.job_list.get_finished()
+
+        self.assertEquals(8, len(finished))
+        self.assertTrue(self.completed_job in finished)
+        self.assertTrue(self.completed_job2 in finished)
+        self.assertTrue(self.completed_job3 in finished)
+        self.assertTrue(self.completed_job4 in finished)
+        self.assertTrue(self.failed_job in finished)
+        self.assertTrue(self.failed_job2 in finished)
+        self.assertTrue(self.failed_job3 in finished)
+        self.assertTrue(self.failed_job4 in finished)
+
+    def test_get_active_returns_only_which_are_in_queue_ready_and_unknown(self):
+        active = self.job_list.get_active()
+
+        self.assertEquals(10, len(active))
+        self.assertTrue(self.queuing_job in active)
+        self.assertTrue(self.running_job in active)
+        self.assertTrue(self.running_job2 in active)
+        self.assertTrue(self.submitted_job in active)
+        self.assertTrue(self.submitted_job2 in active)
+        self.assertTrue(self.submitted_job3 in active)
+        self.assertTrue(self.ready_job in active)
+        self.assertTrue(self.ready_job2 in active)
+        self.assertTrue(self.ready_job3 in active)
+        self.assertTrue(self.unknown_job in active)
+
+    def test_get_job_by_name_returns_the_expected_job(self):
+        job = self.job_list.get_job_by_name(self.completed_job.name)
+
+        self.assertEquals(self.completed_job, job)
+
     def _createDummyJobWithStatus(self, status):
         job_name = 'random-name'
         job_id = randrange(1, 999)
