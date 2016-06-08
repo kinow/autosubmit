966fef54a674eef3f911a25d6b4fba4c21fb2607
jlope2 <joan.lopez@bsc.es>
2016-06-08 15:16:58 +0200

    Fixed setup check test

--- test/unit/test_setup.py
+++ test/unit/test_setup.py
@@ -4,5 +4,13 @@ from unittest import TestCase
 
 class TestSetup(TestCase):
     def test_setup_check_works(self):
-        exit_code = os.system('python ../setup.py check')
+        exit_code = run_setup_check()
         self.assertEquals(0, exit_code)
+
+
+def run_setup_check():
+    return os.system('python ' + get_directory_of_this_file() + '/../../setup.py check')
+
+
+def get_directory_of_this_file():
+    return os.path.dirname(os.path.realpath(__file__))
