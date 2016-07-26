cc21821bbfc9dd8641c3a32d013afb3dc45f9f62
jlope2 <joan.lopez@bsc.es>
2016-07-26 13:32:46 +0200

    File LOG LEVEL defined for create & run commands

--- test/regression/tests_commands.py
+++ test/regression/tests_commands.py
@@ -6,11 +6,11 @@ def generate_experiment_cmd(hpc, description):
 
 
 def create_experiment_cmd(experiment_id):
-    return 'autosubmit create ' + experiment_id + ' -np'
+    return 'autosubmit -lf EVERYTHING create ' + experiment_id + ' -np'
 
 
 def run_experiment_cmd(experiment_id):
-    return 'autosubmit run ' + experiment_id
+    return 'autosubmit -lf EVERYTHING run ' + experiment_id
 
 
 def create_database_cmd():
