5872fb973830ee7654e76a480de4cc47d65295a4
jlope2 <joan.lopez@bsc.es>
2016-11-15 09:04:25 +0100

    Autosubmit 'refresh' fixed. Fixes #211

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -1451,6 +1451,7 @@ class Autosubmit:
         Log.set_file(os.path.join(BasicConfig.LOCAL_ROOT_DIR, expid, BasicConfig.LOCAL_TMP_DIR,
                                   'refresh.log'))
         as_conf = AutosubmitConfig(expid, BasicConfig, ConfigParserFactory())
+        as_conf.reload()
         if not as_conf.check_expdef_conf():
             Log.critical('Can not copy with invalid configuration')
             return False
