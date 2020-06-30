868246ec5f8b71d29ca8a4111d2afbf0a027f68d
dbeltran <dbeltran@bsc.es>
2020-06-30 14:55:33 +0200

    Restructured added failed jobs retrial

--- autosubmit/job/job_packager.py
+++ autosubmit/job/job_packager.py
@@ -25,8 +25,7 @@ from autosubmit.job.job_packages import JobPackageSimple, JobPackageVertical, Jo
 from operator import attrgetter
 from math import ceil
 import operator
-from os import sleep
-
+from time import os 
 class JobPackager(object):
     """
     Main class that manages Job wrapping.
