c479212bcbdbc8cabfd3427304b52b6541e80bcc
dbeltran <dbeltran@bsc.es>
2020-08-13 09:48:24 +0200

    pipeline fix

--- test/unit/test_autosubmit_config.py
+++ test/unit/test_autosubmit_config.py
@@ -393,14 +393,14 @@ class TestAutosubmitConfig(TestCase):
         config.check_platforms_conf = truth_mock
         config.check_jobs_conf = truth_mock
         config.check_expdef_conf = truth_mock
-
+        config.check_proj = truth_mock
         config2 = AutosubmitConfig(self.any_expid, FakeBasicConfig, ConfigParserFactory())
         config2.reload()
         config2.check_autosubmit_conf = truth_mock
         config2.check_platforms_conf = truth_mock
         config2.check_jobs_conf = truth_mock
         config2.check_expdef_conf = Mock(return_value=False)
-
+        config.check_proj = truth_mock
         # act
         should_be_true = config.check_conf_files()
         should_be_false = config2.check_conf_files()
