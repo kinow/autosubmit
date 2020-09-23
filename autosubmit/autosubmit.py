871f0c3de8cb49fdf0c55d1c658023a8158fc118
dbeltran <dbeltran@bsc.es>
2020-09-23 15:00:14 +0200

    more log clarity

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -135,7 +135,7 @@ class Autosubmit:
                                 default='WARNING', type=str,
                                 help="sets file's log level.")
             parser.add_argument('-lc', '--logconsole', choices=('NO_LOG','INFO','WARNING', 'DEBUG'),
-                                default='WARNING', type=str,
+                                default='INFO', type=str,
                                 help="sets console's log level")
 
             subparsers = parser.add_subparsers(dest='command')
