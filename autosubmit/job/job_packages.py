d8cf1072f27307f60e337828cb115a95e3b08275
dbeltran <dbeltran@bsc.es>
2020-10-27 11:26:21 +0100

    PipeLine Fix (had to recover a corrupt file)

--- autosubmit/job/job_packages.py
+++ autosubmit/job/job_packages.py
@@ -75,7 +75,7 @@ class JobPackageBase(object):
         :return: platform
         :rtype: Platform
         """
-        return self.platform
+        return self._platform
 
     def submit(self, configuration, parameters,only_generate=False,hold=False):
         """
