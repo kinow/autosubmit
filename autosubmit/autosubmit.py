07f246fbcd05b296cc8db7627b236615e8ca5f8f
dbeltran <dbeltran@bsc.es>
2020-11-19 12:03:22 +0100

    safetytimer

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -4175,7 +4175,7 @@ class Autosubmit:
         as_conf.set_version(autosubmit_version)
         as_conf.set_expid(exp_id)
         as_conf.set_platform(hpc)
-        as_conf.set_safetysleeptime(10)
+        #as_conf.set_safetysleeptime(10)
 
         if dummy:
             content = open(as_conf.experiment_file).read()
