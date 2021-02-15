f7512c6089df12b666b48afbd978ad515a4cfca5
dbeltran <dbeltran@bsc.es>
2021-02-15 10:02:02 +0100

    added banner timeout

--- autosubmit/platforms/paramiko_platform.py
+++ autosubmit/platforms/paramiko_platform.py
@@ -152,10 +152,10 @@ class ParamikoPlatform(Platform):
                 self._proxy = paramiko.ProxyCommand(
                     self._host_config['proxycommand'])
                 self._ssh.connect(self._host_config['hostname'], 22, username=self.user,
-                                  key_filename=self._host_config_id, sock=self._proxy)
+                                  key_filename=self._host_config_id, sock=self._proxy, banner_timeout=200)
             else:
                 self._ssh.connect(self._host_config['hostname'], 22, username=self.user,
-                                  key_filename=self._host_config_id)
+                                  key_filename=self._host_config_id, banner_timeout=200)
             self.transport = paramiko.Transport(
                 (self._host_config['hostname'], 22))
             self.transport.connect(username=self.user)
