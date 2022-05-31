806a08e130971638f11b1c86dbcf82c4db7968d0
dbeltran <daniel.beltran@bsc.es>
2022-05-31 12:45:53 +0200

    test

--- test/run_unit_suite.py
+++ test/run_unit_suite.py
@@ -1,4 +1,4 @@
 import os
-
-os.system("python3 -m 'nose' --exclude=regression --with-coverage --cover-package=autosubmit --cover-inclusive --cover-xml --cover-xml-file=test/coverage.xml test")
+#--with-coverage --cover-package=autosubmit --cover-inclusive --cover-xml --cover-xml-file=test/coverage.xml
+os.system("python3 -m 'nose' --exclude=regression  test")
 
