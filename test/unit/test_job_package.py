5b09f3c38ab0e3cce4f55dc7b0caa0cfa39b2216
dbeltran <daniel.beltran@bsc.es>
2022-07-01 14:48:34 +0200

    Pipeline fix

--- test/unit/test_job_package.py
+++ test/unit/test_job_package.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 import os
-from mock import Mock
+from mock import Mock,MagicMock
 from mock import patch
 
 from autosubmit.job.job_packages import JobPackageSimple
@@ -12,7 +12,7 @@ from autosubmit.job.job_common import Status
 class TestJobPackage(TestCase):
 
     def setUp(self):
-        self.platform = Mock()
+        self.platform = MagicMock()
         self.jobs = [Job('dummy1', 0, Status.READY, 0),
                      Job('dummy2', 0, Status.READY, 0)]
         self.jobs[0]._platform = self.jobs[1]._platform = self.platform
@@ -23,16 +23,16 @@ class TestJobPackage(TestCase):
             JobPackageSimple([])
 
     def test_job_package_different_platforms_init(self):
-        self.jobs[0]._platform = Mock()
-        self.jobs[1]._platform = Mock()
+        self.jobs[0]._platform = MagicMock()
+        self.jobs[1]._platform = MagicMock()
         with self.assertRaises(Exception):
-            JobPackageSimple(this.jobs)
+            JobPackageSimple(self.jobs)
 
     def test_job_package_none_platforms_init(self):
         self.jobs[0]._platform = None
         self.jobs[1]._platform = None
         with self.assertRaises(Exception):
-            JobPackageSimple(this.jobs)
+            JobPackageSimple(self.jobs)
 
     def test_job_package_length(self):
         self.assertEqual(2, len(self.job_package))
@@ -45,17 +45,17 @@ class TestJobPackage(TestCase):
 
     def test_job_package_submission(self):
         # arrange
-        write_mock = Mock().write = Mock()
+        write_mock = MagicMock().write = MagicMock()
 
         for job in self.jobs:
-            job._tmp_path = Mock()
-            job._get_paramiko_template = Mock("false","empty")
+            job._tmp_path = MagicMock()
+            job._get_paramiko_template = MagicMock("false","empty")
 
-        self.job_package._create_scripts = Mock()
-        self.job_package._send_files = Mock()
-        self.job_package._do_submission = Mock()
+        self.job_package._create_scripts = MagicMock()
+        self.job_package._send_files = MagicMock()
+        self.job_package._do_submission = MagicMock()
         for job in self.jobs:
-            job.update_parameters = Mock()
+            job.update_parameters = MagicMock()
         # act
         self.job_package.submit('fake-config', 'fake-params')
         # assert
