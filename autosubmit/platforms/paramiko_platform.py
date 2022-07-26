7b12e0743977ab9bb48188137764f4c5436c7b4d
dbeltran <daniel.beltran@bsc.es>
2022-07-26 11:19:19 +0200

    added cancel job

--- autosubmit/platforms/paramiko_platform.py
+++ autosubmit/platforms/paramiko_platform.py
@@ -495,7 +495,11 @@ class ParamikoPlatform(Platform):
                     job.start_time = datetime.datetime.now() # URi: start time
                 if job.start_time is not None and str(job.wrapper_type).lower() == "none":
                     if job.is_over_wallclock(job.start_time, job.wallclock):
-                        job_status = Status.FAILED
+                        try:
+                            self.cancel_job(job)
+                            job_status = Status.FAILED
+                        except:
+                            pass
             elif job_status in self.job_status['QUEUING'] and job.hold is False:
                 job_status = Status.QUEUING
             elif job_status in self.job_status['QUEUING'] and job.hold is True:
