1be0a772dd13819f6e999df304e797ac596cb919
dbeltran <daniel.beltran@bsc.es>
2023-05-05 12:54:40 +0200

    Added regression test ( locally )

---
+++ test/regression/local_asparser_test.py
@@ -0,0 +1,61 @@
+"""
+This test checks that the autosubmit report command works as expected.
+It is a regression test, so it is not run by default.
+It only run within my home desktop computer. It is not run in the CI. Eventually it will be included TODO
+Just to be sure that the autosubmitconfigparser work as expected if there are changes.
+"""
+
+import subprocess
+import os
+from pathlib import Path
+BIN_PATH = '../../bin'
+
+def check_cmd(command, path=BIN_PATH):
+    try:
+        output = subprocess.check_output(os.path.join(path, command), shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+    return output
+
+def report_test(expid):
+    output = check_cmd("autosubmit report {0} -all -v".format(expid))
+    return output
+output = report_test("a009")
+report_file = output.decode("UTF-8").split("list of all parameters has been written on ")[1]
+report_file = report_file.split(".txt")[0] + ".txt"
+list_of_parameters_to_find = """
+DEFAULT.CUSTOM_CONFIG.PRE
+DEFAULT.CUSTOM_CONFIG.POST
+DIRECTORIES.INDIR
+DIRECTORIES.OUTDIR
+DIRECTORIES.TESTDIR
+TESTKEY
+TESTKEY-TWO
+TESTKEY-LEVANTE
+PLATFORMS.LEVANTE-LOGIN.USER
+PLATFORMS.LEVANTE-LOGIN.PROJECT
+PLATFORMS.LEVANTE.USER
+PLATFORMS.LEVANTE.PROJECT
+""".split("\n")
+expected_output ="""
+DIRECTORIES.INDIR=my-updated-indir
+DIRECTORIES.OUTDIR=-
+DIRECTORIES.TESTDIR=another-dir
+TESTKEY=abcd
+TESTKEY-TWO=HPCARCH is levante
+TESTKEY-LEVANTE=L-abcd
+PLATFORMS.LEVANTE-LOGIN.USER=b382351
+PLATFORMS.LEVANTE-LOGIN.PROJECT=bb1153
+PLATFORMS.LEVANTE.USER=b382351
+PLATFORMS.LEVANTE.PROJECT=bb1153
+""".split("\n")
+for line in Path(report_file).read_text().split("\n"):
+    if line.split("=")[0] in list_of_parameters_to_find[1:-1]:
+        if line in expected_output:
+            print("OK: " + line)
+        else:
+            for error_line in expected_output:
+                if line.split("=")[0] in error_line:
+                    print(f"ERR: {line.split('=')[0]} loaded value is {line.split('=')[1]} EXPECTED: " + error_line)
+                    break
+
