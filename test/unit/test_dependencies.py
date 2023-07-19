055c43bcbedf10c3abb6dcfc02c05a83772acb25
dbeltran <daniel.beltran@bsc.es>
2023-07-19 14:56:44 +0200

    Some rework, may be finally ready to review

--- test/unit/test_dependencies.py
+++ test/unit/test_dependencies.py
@@ -44,8 +44,8 @@ class TestJobList(unittest.TestCase):
                                 JobListPersistenceDb(self.temp_directory, 'db'), self.as_conf)
         self.date_list = ["20020201", "20020202", "20020203", "20020204", "20020205", "20020206", "20020207", "20020208", "20020209", "20020210"]
         self.member_list = ["fc1", "fc2", "fc3", "fc4", "fc5", "fc6", "fc7", "fc8", "fc9", "fc10"]
-        self.chunk_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-        self.split_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        self.chunk_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.split_list = [1, 2, 3, 4, 5]
         self.JobList._date_list = self.date_list
         self.JobList._member_list = self.member_list
         self.JobList._chunk_list = self.chunk_list
@@ -58,9 +58,9 @@ class TestJobList(unittest.TestCase):
                     "20020201": {
                         "MEMBERS_FROM": {
                             "fc2": {
-                                "DATES_TO": "20020201",
+                                "DATES_TO": "[20020201:20020202]*,20020203",
                                 "MEMBERS_TO": "fc2",
-                                "CHUNKS_TO": "ALL"
+                                "CHUNKS_TO": "all"
                             }
                         },
                         "SPLITS_FROM": {
@@ -72,7 +72,7 @@ class TestJobList(unittest.TestCase):
                 }
             }
         self.relationships_dates_optional = deepcopy(self.relationships_dates)
-        self.relationships_dates_optional["DATES_FROM"]["20020201"]["MEMBERS_FROM"] = { "fc2?": { "DATES_TO": "20020201", "MEMBERS_TO": "fc2", "CHUNKS_TO": "ALL", "SPLITS_TO": "5" } }
+        self.relationships_dates_optional["DATES_FROM"]["20020201"]["MEMBERS_FROM"] = { "fc2?": { "DATES_TO": "20020201", "MEMBERS_TO": "fc2", "CHUNKS_TO": "all", "SPLITS_TO": "5" } }
         self.relationships_dates_optional["DATES_FROM"]["20020201"]["SPLITS_FROM"] = { "ALL": { "SPLITS_TO": "1?" } }
 
         self.relationships_members = {
@@ -82,7 +82,7 @@ class TestJobList(unittest.TestCase):
                             "ALL": {
                                 "DATES_TO": "20020201",
                                 "MEMBERS_TO": "fc2",
-                                "CHUNKS_TO": "ALL",
+                                "CHUNKS_TO": "all",
                                 "SPLITS_TO": "1"
                             }
                         }
@@ -94,7 +94,7 @@ class TestJobList(unittest.TestCase):
                     "1": {
                         "DATES_TO": "20020201",
                         "MEMBERS_TO": "fc2",
-                        "CHUNKS_TO": "ALL",
+                        "CHUNKS_TO": "all",
                         "SPLITS_TO": "1"
                     }
                 }
@@ -104,7 +104,7 @@ class TestJobList(unittest.TestCase):
                     "1": {
                         "DATES_TO": "20020201",
                         "MEMBERS_TO": "fc2",
-                        "CHUNKS_TO": "ALL",
+                        "CHUNKS_TO": "all",
                         "SPLITS_TO": "1"
                     },
                     "2": {
@@ -122,7 +122,7 @@ class TestJobList(unittest.TestCase):
                     "1": {
                         "DATES_TO": "20020201",
                         "MEMBERS_TO": "fc2",
-                        "CHUNKS_TO": "ALL",
+                        "CHUNKS_TO": "all",
                         "SPLITS_TO": "1"
                     }
                 }
@@ -131,7 +131,7 @@ class TestJobList(unittest.TestCase):
         self.relationships_general = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         # Create a mock Job object
@@ -146,6 +146,36 @@ class TestJobList(unittest.TestCase):
         self.mock_job.member = None
         self.mock_job.chunk = None
         self.mock_job.split = None
+
+    def test_unify_to_filter(self):
+        """Test the _unify_to_fitler function"""
+        # :param unified_filter: Single dictionary with all filters_to
+        # :param filter_to: Current dictionary that contains the filters_to
+        # :param filter_type: "DATES_TO", "MEMBERS_TO", "CHUNKS_TO", "SPLITS_TO"
+        # :return: unified_filter
+        unified_filter = \
+            {
+                "DATES_TO": "20020201",
+                "MEMBERS_TO": "fc2",
+                "CHUNKS_TO": "all",
+                "SPLITS_TO": "1"
+            }
+        filter_to = \
+            {
+                "DATES_TO": "20020205,[20020207:20020208]",
+                "MEMBERS_TO": "fc2,fc3",
+                "CHUNKS_TO": "all"
+            }
+        filter_type = "DATES_TO"
+        result = self.JobList._unify_to_filter(unified_filter, filter_to, filter_type)
+        expected_output = \
+            {
+                "DATES_TO": "20020201,20020205,20020207,20020208,",
+                "MEMBERS_TO": "fc2",
+                "CHUNKS_TO": "all",
+                "SPLITS_TO": "1"
+            }
+        self.assertEqual(result, expected_output)
 
     def test_simple_dependency(self):
         result_d = self.JobList._check_dates({}, self.mock_job)
@@ -202,6 +232,19 @@ class TestJobList(unittest.TestCase):
         result = self.JobList._parse_filter_to_check("[:20020203:]",self.date_list)
         expected_output = self.date_list[:3]
         self.assertEqual(result, expected_output)
+        # test with a member N:N
+        result = self.JobList._parse_filter_to_check("[fc2:fc3]",self.member_list)
+        expected_output = ["fc2","fc3"]
+        self.assertEqual(result, expected_output)
+        # test with a chunk
+        result = self.JobList._parse_filter_to_check("[1:2]",self.chunk_list,level_to_check="CHUNKS_FROM")
+        expected_output = [1,2]
+        self.assertEqual(result, expected_output)
+        # test with a split
+        result = self.JobList._parse_filter_to_check("[1:2]",self.split_list,level_to_check="SPLITS_FROM")
+        expected_output = [1,2]
+        self.assertEqual(result, expected_output)
+
 
     def test_check_dates(self):
         # Call the function to get the result
@@ -211,15 +254,13 @@ class TestJobList(unittest.TestCase):
         self.mock_job.split = 1
         result = self.JobList._check_dates(self.relationships_dates, self.mock_job)
         expected_output = {
-                "DATES_TO": "20020201",
+                "DATES_TO": "20020201*,20020202*,20020203",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.assertEqual(result, expected_output)
-        self.mock_job.date = datetime.strptime("20020202", "%Y%m%d")
-        result = self.JobList._check_dates(self.relationships_dates, self.mock_job)
-        self.assertEqual(result, {})
+
     def test_check_members(self):
         # Call the function to get the result
         self.mock_job.date = datetime.strptime("20020201", "%Y%m%d")
@@ -229,13 +270,14 @@ class TestJobList(unittest.TestCase):
         expected_output = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.assertEqual(result, expected_output)
         self.mock_job.member = "fc3"
         result = self.JobList._check_members(self.relationships_members, self.mock_job)
         self.assertEqual(result, {})
+
 
     def test_check_splits(self):
         # Call the function to get the result
@@ -245,13 +287,14 @@ class TestJobList(unittest.TestCase):
         expected_output = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.assertEqual(result, expected_output)
         self.mock_job.split = 2
         result = self.JobList._check_splits(self.relationships_splits, self.mock_job)
         self.assertEqual(result, {})
+
     def test_check_chunks(self):
         # Call the function to get the result
 
@@ -260,23 +303,23 @@ class TestJobList(unittest.TestCase):
         expected_output = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.assertEqual(result, expected_output)
         self.mock_job.chunk = 2
         result = self.JobList._check_chunks(self.relationships_chunks, self.mock_job)
         self.assertEqual(result, {})
-        # test splits_from
-        self.mock_job.split = 5
-        result = self.JobList._check_chunks(self.relationships_chunks2, self.mock_job)
-        expected_output2 = {
-                "SPLITS_TO": "2"
-            }
-        self.assertEqual(result, expected_output2)
-        self.mock_job.split = 1
-        result = self.JobList._check_chunks(self.relationships_chunks2, self.mock_job)
-        self.assertEqual(result, {})
+        # # test splits_from
+        # self.mock_job.split = 5
+        # result = self.JobList._check_chunks(self.relationships_chunks2, self.mock_job)
+        # expected_output2 = {
+        #         "SPLITS_TO": "2"
+        #     }
+        # self.assertEqual(result, expected_output2)
+        # self.mock_job.split = 1
+        # result = self.JobList._check_chunks(self.relationships_chunks2, self.mock_job)
+        # self.assertEqual(result, {})
 
     def test_check_general(self):
         # Call the function to get the result
@@ -289,7 +332,7 @@ class TestJobList(unittest.TestCase):
         expected_output = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.assertEqual(result, expected_output)
@@ -297,7 +340,7 @@ class TestJobList(unittest.TestCase):
 
     def test_valid_parent(self):
         # Call the function to get the result
-        date_list = ["20020201"]
+        date_list = ["20020201", "20020202", "20020203", "20020204", "20020205", "20020206", "20020207", "20020208", "20020209", "20020210"]
         member_list = ["fc1", "fc2", "fc3"]
         chunk_list = [1, 2, 3]
         self.mock_job.splits = 10
@@ -306,7 +349,7 @@ class TestJobList(unittest.TestCase):
         filter_ = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         # PArent job values
@@ -320,7 +363,7 @@ class TestJobList(unittest.TestCase):
         filter_ = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1?"
             }
         result = self.JobList._valid_parent(self.mock_job, member_list, date_list, chunk_list, is_a_natural_relation, filter_)
@@ -328,7 +371,7 @@ class TestJobList(unittest.TestCase):
         filter_ = {
                 "DATES_TO": "20020201",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1?"
             }
         self.mock_job.split = 2
@@ -338,7 +381,7 @@ class TestJobList(unittest.TestCase):
         filter_ = {
                 "DATES_TO": "[20020201:20020205]",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.mock_job.split = 1
@@ -347,7 +390,7 @@ class TestJobList(unittest.TestCase):
         filter_ = {
                 "DATES_TO": "[20020201:20020205]",
                 "MEMBERS_TO": "fc2",
-                "CHUNKS_TO": "ALL",
+                "CHUNKS_TO": "all",
                 "SPLITS_TO": "1"
             }
         self.mock_job.date = datetime.strptime("20020206", "%Y%m%d")
@@ -362,6 +405,17 @@ class TestJobList(unittest.TestCase):
         self.mock_job.date = datetime.strptime("20020201", "%Y%m%d")
         self.mock_job.chunk = 2
         self.mock_job.split = 1
+        result = self.JobList._valid_parent(self.mock_job, member_list, date_list, chunk_list, is_a_natural_relation, filter_)
+        self.assertEqual(result, True)
+        filter_ = {
+                "DATES_TO": "[20020201:20020202],20020203,20020204,20020205",
+                "MEMBERS_TO": "fc2",
+                "CHUNKS_TO": "[2:4]*,5",
+                "SPLITS_TO": "[1:5],6"
+            }
+        self.mock_job.date = datetime.strptime("20020204", "%Y%m%d")
+        self.mock_job.chunk = 5
+        self.mock_job.split = 6
         result = self.JobList._valid_parent(self.mock_job, member_list, date_list, chunk_list, is_a_natural_relation, filter_)
         self.assertEqual(result, True)
 
