a8f02bf2b3b24d98dd178649010d8d5edbf98b3b
dbeltran <daniel.beltran@bsc.es>
2024-03-08 17:11:53 +0100

    Removed argparser changed print for log.info

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -646,7 +646,7 @@ class Autosubmit:
 
             args, unknown = parser.parse_known_args()
             if args.version:
-                print(Autosubmit.autosubmit_version)
+                Log.info(Autosubmit.autosubmit_version)
                 return 0
             if unknown or args.command is None:
                 parser.print_help()
