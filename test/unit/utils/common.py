25fbb0a05bdce41b1346fdbd67bfacb6e0cc2606
dbeltran <daniel.beltran@bsc.es>
2024-11-06 16:27:08 +0100

    Delete improvements fixes #1317.

--- test/unit/utils/common.py
+++ test/unit/utils/common.py
@@ -6,13 +6,16 @@ def create_database(envirom):
     BasicConfig.read()
     Autosubmit.install()
 
-def init_expid(envirom, platform="local", expid=None, create=True):
+
+def init_expid(envirom, platform="local", expid=None, create=True, test_type="normal"):
     os.environ['AUTOSUBMIT_CONFIGURATION'] = envirom
     if not expid:
-        expid = Autosubmit.expid("pytest", hpc=platform, copy_id='', dummy=True, minimal_configuration=False, git_repo="", git_branch="", git_as_conf="", operational=False,  testcase = True, use_local_minimal=False)
+        if test_type == "normal":
+            expid = Autosubmit.expid("pytest", hpc=platform, copy_id='', dummy=True, minimal_configuration=False, git_repo="", git_branch="", git_as_conf="", operational=False,  testcase = False, use_local_minimal=False)
+        elif test_type == "test":
+            expid = Autosubmit.expid("pytest", hpc=platform, copy_id='', dummy=True, minimal_configuration=False, git_repo="", git_branch="", git_as_conf="", operational=False,  testcase = True, use_local_minimal=False)
+        elif test_type == "operational":
+            expid = Autosubmit.expid("pytest", hpc=platform, copy_id='', dummy=True, minimal_configuration=False, git_repo="", git_branch="", git_as_conf="", operational=True,  testcase = False, use_local_minimal=False)
     if create:
         Autosubmit.create(expid, True,False, force=True)
     return expid
-
-
-
