fec13bd644260b5acf9b89f0a953f500b3b696cd
Irene Simo Munoz <irene.simomunoz@bsc.es>
2025-02-06 14:05:18 +0100

    Fix version text

--- test/unit/test_version.py
+++ test/unit/test_version.py
@@ -6,12 +6,12 @@ from autosubmit.autosubmit import Autosubmit
 
 
 def test_autosubmit_version():
-    bin_path = Path(__file__, '../../../bin/autosubmit').resolve()
+    bin_path = Path(__file__, '../../../autosubmit/scripts/bin_autosubmit.py').resolve()
     exit_code, out = subprocess.getstatusoutput(' '.join([sys.executable, str(bin_path), '-v']))
     assert exit_code == 0
     assert out.strip().endswith(Autosubmit.autosubmit_version)
 
 def test_autosubmit_version_broken():
-    bin_path = Path(__file__, '../../../bin/autosubmit').resolve()
+    bin_path = Path(__file__, '../../../autosubmit/scripts/bin_autosubmit.py').resolve()
     exit_code, _ = subprocess.getstatusoutput(' '.join([sys.executable, str(bin_path), '-abcdefg']))
     assert exit_code == 1
