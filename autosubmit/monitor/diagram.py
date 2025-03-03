51c2d1d597ebf7fb7f92096393746d431558a9a9
Erick Lopes <30758033+VindeeR@users.noreply.github.com>
2025-03-03 17:23:38 +0100

    Test job data (#2168)
    * first commit of the tes function for diagram + fix linting
    
    * test for Job Data class
    
    * adjusting description to be in accordance to PEP

--- autosubmit/monitor/diagram.py
+++ autosubmit/monitor/diagram.py
@@ -17,9 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Autosubmit.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-Diagram generator File
-"""
+""" Diagram generator File """
 
 import itertools
 import math
--- test/unit/test_diagram.py
+++ test/unit/test_diagram.py
@@ -16,14 +16,22 @@
 
 # You should have received a copy of the GNU General Public License
 # along with Autosubmit.  If not, see <http://www.gnu.org/licenses/>.
-"""
-Test file for autosubmit/monitor/diagram.py
-"""
+
+""" Test file for autosubmit/monitor/diagram.py """
 import datetime
 
-from autosubmit.monitor.diagram import JobAggData
+from autosubmit.monitor.diagram import JobData, JobAggData
 
 
+def test_job_data():
+    """ function to test the Class JobData inside autosubmit/monitor/diagram.py """
+    job_data = JobData()
+
+    assert job_data.headers() == ['Job Name', 'Queue Time', 'Run Time', 'Status']
+    assert job_data.values() == ['', datetime.timedelta(0), datetime.timedelta(0), '']
+    assert job_data.number_of_columns() == 4
+
+    
 def test_job_agg_data():
     """
     function to test the Class JobAggData inside autosubmit/monitor/diagram.py
