42a68801766d2d9af1fd17a7e222b1c74ffc10b0
Erick Lopes <30758033+VindeeR@users.noreply.github.com>
2025-03-17 15:24:45 +0100

    history_db_manager: Adjusting to remove unused varible (#2186)
    * adjusting to remove unused varible in history_db_manager
    
    * adjusting false unused variable
    
    * Update experiment_history_db_manager.py

--- autosubmit/history/database_managers/experiment_history_db_manager.py
+++ autosubmit/history/database_managers/experiment_history_db_manager.py
@@ -450,8 +450,9 @@ class ExperimentHistoryDatabaseManager(Protocol):
 
 
 def create_experiment_history_db_manager(
-    db_engine: str, **options: Any
+    db_engine: str, **options: Any  # noqa: F841
 ) -> ExperimentHistoryDatabaseManager:
+  # pylint: disable=W0613
   return cast(ExperimentHistoryDatabaseManager, ExperimentHistoryDbManager(
     options['schema'],
     options.get('jobdata_dir_path', DEFAULT_JOBDATA_DIR)
