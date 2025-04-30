f54c47e91e69e912a6dc31437fde427c0650d754
Erick Lopes <30758033+VindeeR@users.noreply.github.com>
2025-04-30 15:47:36 +0200

    fixing config file to copy the same HPCARCH existing in base project (#2082)
    * GL-1360: copy the HPCARCH properly when -y is used
    
    * fixing config file to copy the same HPCARCH existing in base project done from the branch master
    
    * padronazing, organizing, and reestructuring test to work on the newer version of the code following the standard formatting
    
    * no reason behind the test in test_autosubmit since the test in the regression is similar and more complete
    
    * small fixes
    Co-authored-by: VindeeR <ericklopes.47@gmail.com>
    Co-authored-by: kinow <>
    
    ---------
    
    Co-authored-by: Edgar Garriga <edgar.garriga@bsc.es>

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -1301,7 +1301,7 @@ class Autosubmit:
         return content
 
     @staticmethod
-    def as_conf_default_values(exp_id,hpc="local",minimal_configuration=False,git_repo="",git_branch="main",git_as_conf=""):
+    def as_conf_default_values(exp_id, hpc, minimal_configuration=False, git_repo="", git_branch="main", git_as_conf=""):
         """
         Replace default values in as_conf files
         :param exp_id: experiment id
@@ -1312,15 +1312,18 @@ class Autosubmit:
         :param git_as_conf: path to as_conf file in git repository
         :return: None
         """
+        # the var hpc was hardcoded in the header of the function
+        hpc_default = "local"
+
         # open and replace values
-        for as_conf_file in os.listdir(os.path.join(BasicConfig.LOCAL_ROOT_DIR, exp_id,"conf")):
-            if as_conf_file.endswith(".yml") or as_conf_file.endswith(".yaml"):
-                with open(os.path.join(BasicConfig.LOCAL_ROOT_DIR, exp_id,"conf", as_conf_file), 'r') as f:
+        for as_conf_file in Path(BasicConfig.LOCAL_ROOT_DIR, f"{exp_id}/conf").iterdir():
+            if as_conf_file.name.endswith(".yml") or as_conf_file.name.endswith(".yaml"):
+                with open(as_conf_file, 'r+') as f:
                     # Copied files could not have default names.
                     content = f.read()
                     search = re.search('AUTOSUBMIT_VERSION: .*', content, re.MULTILINE)
                     if search is not None:
-                        content = content.replace(search.group(0), "AUTOSUBMIT_VERSION: \""+Autosubmit.autosubmit_version+"\"")
+                        content = content.replace(search.group(0), f"AUTOSUBMIT_VERSION: \"{Autosubmit.autosubmit_version}\"")
                     search = re.search('NOTIFICATIONS: .*', content, re.MULTILINE)
                     if search is not None:
                         content = content.replace(search.group(0),"NOTIFICATIONS: False")
@@ -1330,22 +1333,33 @@ class Autosubmit:
                     content = Autosubmit.replace_parameter_inside_section(content, "EXPID", exp_id, "DEFAULT")
                     search = re.search('HPCARCH: .*', content, re.MULTILINE)
                     if search is not None:
-                        content = content.replace(search.group(0),"HPCARCH: \""+hpc+"\"")
+                        x = search.group(0).split(":")
+                        # clean blank space, quotes and double quote
+                        aux = x[1].strip(' "\'')
+                        # hpc in config is empty && -H is empty-> hpc_default will be introduced
+                        if len(aux) == 0 and not hpc:
+                            content = content.replace(search.group(0), f"HPCARCH: \"{hpc_default}\"")
+                        # hpc in config is empty && -H has a value-> write down hpc value
+                        elif len(aux) == 0:
+                            content = content.replace(search.group(0), f"HPCARCH: \"{hpc}\"")
+                        # the other case is aux!=0 that we dont care about val(hpc) because its a copyExpId
                     if minimal_configuration:
                         search = re.search('CUSTOM_CONFIG: .*', content, re.MULTILINE)
                         if search is not None:
                             content = content.replace(search.group(0), "CUSTOM_CONFIG: \"%PROJDIR%/"+git_as_conf+"\"")
                         search = re.search('PROJECT_ORIGIN: .*', content, re.MULTILINE)
                         if search is not None:
-                            content = content.replace(search.group(0), "PROJECT_ORIGIN: \""+git_repo+"\"")
+                            content = content.replace(search.group(0), f"PROJECT_ORIGIN: \"{git_repo}\"")
                         search = re.search('PROJECT_PATH: .*', content, re.MULTILINE)
                         if search is not None:
-                            content = content.replace(search.group(0), "PROJECT_PATH: \""+git_repo+"\"")
+                            content = content.replace(search.group(0), f"PROJECT_PATH: \"{git_repo}\"")
                         search = re.search('PROJECT_BRANCH: .*', content, re.MULTILINE)
                         if search is not None:
-                            content = content.replace(search.group(0), "PROJECT_BRANCH: \""+git_branch+"\"")
-                with open(os.path.join(BasicConfig.LOCAL_ROOT_DIR, exp_id,"conf", as_conf_file), 'w') as f:
+                            content = content.replace(search.group(0), f"PROJECT_BRANCH: \"{git_branch}\"")
+
+                    f.seek(0)
                     f.write(content)
+                    f.truncate()
 
     @staticmethod
     def expid(description, hpc="", copy_id='', dummy=False, minimal_configuration=False, git_repo="", git_branch="", git_as_conf="", operational=False,  testcase = False, evaluation = False, use_local_minimal=False):
@@ -3324,11 +3338,12 @@ class Autosubmit:
         """
         Show details for specified experiment
 
-        :param experiments_id: experiments identifier:
-        :type experiments_id: str
+        :param input_experiment_list: experiments identifier:
+        :type input_experiment_list: str
         :param get_from_user: user to get the experiments from
         :type get_from_user: str
-        :return: str,str,str,str
+        :return: tuple with user, created time, model, branch, and HPC
+        :rtype: bool | (str, str, str, str, str)
         """
         experiments_ids = input_experiment_list
         not_described_experiments = []
@@ -3371,7 +3386,6 @@ class Autosubmit:
                 created = datetime.datetime.fromtimestamp(
                     os.path.getmtime(as_conf.conf_folder_yaml))
 
-                project_type = as_conf.get_project_type()
                 if as_conf.get_svn_project_url():
                     model = as_conf.get_svn_project_url()
                     branch = as_conf.get_svn_project_url()
@@ -4651,6 +4665,7 @@ class Autosubmit:
         :return: True if successful, False if not
         :rtype: bool
         """
+
 
         project_destination = as_conf.get_project_destination()
         if project_destination is None or len(project_destination) == 0:
--- test/integration/test_expid.py
+++ test/integration/test_expid.py
@@ -16,11 +16,13 @@
 # along with Autosubmit.  If not, see <http://www.gnu.org/licenses/>.
 
 """Tests for ``autosubmit expid``."""
-
+from collections.abc import Callable
 from pathlib import Path
-from log.log import AutosubmitCritical
-from autosubmitconfigparser.config.basicconfig import BasicConfig
+
 import pytest
+from autosubmitconfigparser.config.basicconfig import BasicConfig
+from autosubmit.autosubmit import Autosubmit
+from log.log import AutosubmitCritical
 
 
 @pytest.mark.parametrize(
@@ -30,14 +32,24 @@ import pytest
         'ev'
     ]
 )
-def test_copy_experiment(type_flag, autosubmit_exp):
-    """Test that we can copy experiment using flags for operational and evaluation experiment types."""
+def test_copy_experiment(type_flag: str, autosubmit_exp: Callable, autosubmit: Autosubmit) -> None:
+    """Test that we can copy experiment using flags for operational and evaluation experiment types.
+
+    :param type_flag: Variable to check which kind of flag it is.
+    :type type_flag: bool
+    :param autosubmit_exp: Autosubmit interface that instantiate with an experiment.
+    :type autosubmit_exp: Callable
+    :param autosubmit: Autosubmit interface that instantiate with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
     base_experiment = autosubmit_exp('t000', experiment_data={})
 
     is_operational = type_flag == 'op'
     is_evaluation = type_flag == 'ev'
 
-    expid = base_experiment.autosubmit.expid(
+    expid = autosubmit.expid(
         'test',
         hpc='local',
         copy_id=base_experiment.expid,
@@ -47,6 +59,7 @@ def test_copy_experiment(type_flag, autosubmit_exp):
 
     assert expid
 
+
 @pytest.mark.parametrize(
     'type_flag,',
     [
@@ -54,8 +67,16 @@ def test_copy_experiment(type_flag, autosubmit_exp):
         'ev'
     ]
 )
-def test_expid_mutually_exclusive_arguments(type_flag, autosubmit):
-    """Test for issue 2280, where mutually exclusive arguments like op/ev flags and min were not working."""
+def test_expid_mutually_exclusive_arguments(type_flag: str, autosubmit: Autosubmit) -> None:
+    """Test for issue 2280, where mutually exclusive arguments like op/ev flags and min were not working.
+
+    :param type_flag: Variable to check which kind of flag it is.
+    :type type_flag: bool
+    :param autosubmit: Autosubmit interface that instantiate with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
     is_operational = type_flag == 'op'
     is_evaluation = type_flag == 'ev'
 
@@ -69,6 +90,7 @@ def test_expid_mutually_exclusive_arguments(type_flag, autosubmit):
 
     assert expid
 
+
 @pytest.mark.parametrize(
         'has_min_yaml',
         [
@@ -76,19 +98,24 @@ def test_expid_mutually_exclusive_arguments(type_flag, autosubmit):
             False
         ]
 )
-def test_copy_minimal(has_min_yaml, autosubmit_exp, autosubmit):
+def test_copy_minimal(has_min_yaml: bool, autosubmit: Autosubmit) -> None:
     """
     Test for issue 2280, ensure autousbmit expid -min --copy expid_id cannot be used if the experiment
     does not have a expid_id/conf/minimal.yml file
-    """
-    base_experiment = autosubmit_exp('t000', experiment_data={})
 
-    expid = base_experiment.autosubmit.expid(
+    :param has_min_yaml: Variable to simulate if experiment has minimal or not.
+    :type has_min_yaml: bool
+    :param autosubmit: Autosubmit interface that instantiate with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
+
+    expid = autosubmit.expid(
             'test',
             hpc='local',
             minimal_configuration=True
     )
-    
     minimal_file = Path(BasicConfig.LOCAL_ROOT_DIR) / expid / "conf" / "minimal.yml"
     if has_min_yaml:
         minimal_file.write_text("test")
@@ -97,7 +124,6 @@ def test_copy_minimal(has_min_yaml, autosubmit_exp, autosubmit):
                 copy_id=expid,
                 description="Pytest experiment")
         assert expid2
-        
         minimal2 = Path(BasicConfig.LOCAL_ROOT_DIR) / expid2 / "conf" / "minimal.yml"
         content = minimal2.read_text()
         assert content == "test", f"Unexpected content: {content}"
@@ -110,3 +136,127 @@ def test_copy_minimal(has_min_yaml, autosubmit_exp, autosubmit):
                     description="Pytest experiment")
             assert exc_info.value.code == 7011
             assert "minimal.yml" in str(exc_info.value)
+
+
+def test_create_expid_default_hpc(autosubmit: Autosubmit) -> None:
+    """Create expid with the default hcp value (no -H flag defined).
+
+    .. code-block:: console
+
+        autosubmit expid -d "test descript"
+
+    :param autosubmit: Autosubmit interface that instantiate with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
+    # create default expid
+    experiment_id = autosubmit.expid(
+        'experiment_id',
+        "",
+        minimal_configuration=True
+    )
+
+    # capture the platform using the "describe"
+    describe = autosubmit.describe(experiment_id)
+    hpc_result  = describe[4].lower()
+
+    assert hpc_result == "local"
+
+
+@pytest.mark.parametrize("fake_hpc, expected_hpc", [
+    ("mn5", "mn5"),
+    ("", "local"), ])
+def test_create_expid_flag_hpc(fake_hpc: str, expected_hpc: str, autosubmit: Autosubmit) -> None:
+    """Create expid using the flag -H. Defining a value for the flag and not defining any value for that flag.
+
+    .. code-block:: console
+
+        autosubmit expid -H ithaca -d "experiment"
+        autosubmit expid -H "" -d "experiment"
+
+    :param fake_hpc: The value for the -H flag (hpc value).
+    :type fake_hpc: str
+    :param expected_hpc: The value it is expected for the variable hpc.
+    :type expected_hpc: str
+    :param autosubmit: Autosubmit interface that instantiate with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
+    # create default expid with know hpc
+
+    experiment_id = autosubmit.expid(
+        'experiment',
+        fake_hpc,
+        minimal_configuration=True
+    )
+
+    # capture the platform using the "describe"
+    describe_experiment = autosubmit.describe(experiment_id)
+    hpc_result_experiment = describe_experiment[4].lower()
+
+    assert hpc_result_experiment == expected_hpc
+
+
+@pytest.mark.parametrize("fake_hpc, expected_hpc", [
+    ("mn5", "mn5"),
+    ("", "local"),
+])
+def test_copy_expid(fake_hpc: str, expected_hpc: str, autosubmit: Autosubmit) -> None:
+    """Copy an experiment without indicating which is the new HPC platform
+
+    .. code-block:: console
+
+        autosubmit expid -d "original" -H "<PLATFORM>"
+        autosubmit expid -y a000 -d ""
+
+    :param fake_hpc: The value for the -H flag (hpc value).
+    :type fake_hpc: str
+    :param expected_hpc: The value it is expected for the variable hpc.
+    :type expected_hpc: str
+    :param autosubmit: Autosubmit interface that instantiate with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
+    # create default expid with know hpc
+
+    original_id = autosubmit.expid(
+        'original',
+        fake_hpc,
+        minimal_configuration=True
+    )
+
+    copy_id = autosubmit.expid('copy', "", original_id)
+
+    # capture the platform using the "describe"
+    describe_copy = autosubmit.describe(copy_id)
+    hpc_result_copy = describe_copy[4].lower()
+
+    assert hpc_result_copy == expected_hpc
+
+
+def test_copy_expid_no(autosubmit: Autosubmit) -> None:
+    """Copying expid, but choosing another HPC value must create a new experiment with the chosen HPC value
+
+    .. code-block:: console
+
+        autosubmit expid -y a000 -h local -d "experiment is about..."
+
+    :param autosubmit: Autosubmit interface that instantiates with no experiment.
+    :type autosubmit: Autosubmit
+
+    :return: None
+    """
+    # create default expid with know hpc
+    fake_hpc = "mn5"
+    new_hpc = "local"
+
+    experiment_id = autosubmit.expid('original', fake_hpc)
+    copy_experiment_id = autosubmit.expid("copy experiment", new_hpc, experiment_id)
+    # capture the platform using the "describe"
+    describe = autosubmit.describe(copy_experiment_id)
+    hpc_result = describe[4].lower()
+
+    assert hpc_result != new_hpc
