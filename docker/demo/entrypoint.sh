7eb0cee551dc3feb367d73f30b3a1222b8430ae6
Luiggi Tenorio <luiggibit@gmail.com>
2025-06-10 13:23:06 +0200

    add Docker option to run with postgres (#2406)

--- docker/demo/entrypoint.sh
+++ docker/demo/entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 # Prepare Autosubmit
-autosubmit configure
+autosubmit configure $( [ -n "$AS_PG_CONN_URL" ] && echo "--database-backend=postgres --database-conn-url=$AS_PG_CONN_URL" )
 autosubmit install
 
 # Execute /load_ssh_private_key.sh
 /load_ssh_private_key.sh
 
 # Run jupyter lab as daemon and assign token if env variable exists
-if [ -n "$JUPYTER_TOKEN" ]; then
-    jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.base_url=/jupyterlab --NotebookApp.token=$JUPYTER_TOKEN &
-else
-    jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.base_url=/jupyterlab &
-fi
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.base_url=/jupyterlab $( [ -n "$JUPYTER_TOKEN" ] && echo "--NotebookApp.token=$JUPYTER_TOKEN" ) &
 
 # Run the command passed by docker run
 /apps/autosubmit-api/bin/autosubmit_api start -b 0.0.0.0:8000
